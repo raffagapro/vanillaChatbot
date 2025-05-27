5ef7f7cf4a3a1a2b0824185707c30d1da9e4aa88
raffagapro <raffagapro@gmail.com>
2025-05-26 19:43:29 -0600

    first working implementation

--- weather/index.js
+++ weather/index.js
@@ -4,15 +4,23 @@ const axios = require('axios');
 const getWeather = location => {
     return new Promise(async (resolve, reject) => {
         try {
-            const weatherConditions = await axios.get(
-                'https://api.weatherapi.com/v1/current.json',
+            location = location.replace("?", "");
+            location = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?parameters`,
                 {
                     params:{
-                        key: process.env.WEATHER_API_KEY,
-                        q: location,
-                        days:3
+                        key:process.env.WEATHER_API_KEY,
+                        address:location
                     }
                 }
+            ).then(res =>{
+                //NEED OPTIMIZATION FOR WHEN THERE IS MORE THAN 1 RESULT
+                return {
+                    latitude: res.data.results[0].geometry.location.lat,
+                    longitude: res.data.results[0].geometry.location.lng
+                }
+            })
+            const weatherConditions = await axios.get(
+                `https://weather.googleapis.com/v1/currentConditions:lookup?key=${process.env.WEATHER_API_KEY}&location.latitude=${location.latitude}&location.longitude=${location.longitude}`
             );
             resolve(weatherConditions.data);
         } catch (error) {
